ad60c9da1f93cf8ccd122fc2b162e64059882fad
AtoliH <atoli.huppe@ensta-paris.fr>
2020-04-12 21:16:02 +0200

    Add autosave.py to .gitignore

--- tests/ai_test.py
+++ tests/ai_test.py
@@ -11,3 +11,12 @@ def test_RandomAI():
     # pour jouer toute une partie
     game.ai = ai
     game.run()
+
+
+def test_Cheater():
+    game = hanabi.Game(2)  # 2 players
+    ai = hanabi.ai.Cheater(game)
+
+    # pour jouer toute une partie
+    game.ai = ai
+    game.run()
