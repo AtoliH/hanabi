f9470b087819c5adc189b3dfcd25c2402390dcf2
Yon <guillaume.yon@ensta-paris.fr>
2020-05-01 17:57:01 +0200

    Méthode other_players_actions qui retourne la liste des actions que doivent effectuer les autres joueurs

--- src/hanabi/recommandation_ai.py
+++ src/hanabi/recommandation_ai.py
@@ -36,6 +36,10 @@ class Recommandation(hanabi.ai.AI):
 
 
 	def other_players_actions(self):
+
+		#Retourne une liste de couples, un pour chaque joueur, comprenant l'indice de la carte (en commençant à 0) et une lettre 
+		#déterminant s'il faut la jouer ou la défausser
+		
 		game = self.game
 
 		other_hands = self.other_hands  
@@ -47,25 +51,84 @@ class Recommandation(hanabi.ai.AI):
 		for i in range(len(other_hands)):
 
 			#Liste des cartes jouables par le joueur i
-			self.playable = [(k, card.number) for (k, card)
-			in enumerate(self.other_hands.cards)
-			if self.list_status[i][k] == 1 ]
+			playable = []
+			for k in range(len(other_hands[0])):
+				card = other_hands[i].cards[k]
+				if self.list_status[i][k] == 1 :
+					playable.append((k, card.number))
+
+		
+			(j, l) = (0, 0)
+
+
+			if playable :
+
+				
+				while j < len(playable) and l == 0:
+					card_number = playable[j][1]
+					if card_number == 5 :
+						self.actions.append((playable[j][0], 'p')) #Indice de la carte trouvée et lettre déterminant si elle doit être jouée ou défaussée
+						l = 1
+					j += 1
+
+
+				if l == 0: #On a pas trouvé de carte de valeur 5 qui soit jouable
+					max_card_number = playable[0][1]
+					card_index = playable[0][0]
+					for p in range(len(playable)):
+						if playable[p][1] > max_card_number:
+							card_index = playable[p][0]
+					l = 1
+					self.actions.append((card_index, 'p'))
+
+			else :
+
+				discardable = [] #Liste des cartes défaussables
+
+				for a in range(len(other_hands[0])):
+					card = other_hands[i].cards[a]
+					if self.list_status[i][a] == 0:
+						discardable.append((a, card.number))
+
+				if discardable :
+					self.actions.append((discardable[0][0], 'd')) #Défausser la carte inutile de plus petit indice
+
+				else :
+
+					non_indisp = [] #Liste des cartes non-indispensables
+
+					for b in range(len(other_hands[0])):
+						card = other_hands[i].cards[b]
+						if self.list_status[i][b] == 3:
+							non_indisp.append((b, card.number))
+
+					if non_indisp :
+						max_card_number = non_indisp[0][1]
+						card_index = non_indisp[0][0]
+						for c in range(len(non_indisp)):
+							if non_indisp[c][1] > max_card_number :
+								card_index = non_indisp[c][0]
+						self.actions.append((card_index, 'd')) #Carte non_indispensable de plus haute valeur
+
+					else :
+						self.actions.append((0, 'd'))  #Aucune carte n'a vérifié les conditions, on défausse c1 par défaut
+
+
+	def play(self) :
+
+		game = self.game
+		self.card_status()
+		self.other_players_actions()
+
+		
+
+				
 
 
 
-			for j in range(len (other_hands[0])):
 
-				card = other_hands[i].cards[j]
 
-				if card.number == 5 and self.list_status[i][j] == 1: # 5 jouable immédiatement
-					self.actions.append(j)
 
-				elif self.list_status[i][j] == 0: # Carte inutile de plus petit indice
-					self.actions.append(j+3)
-
-				else:
-					pass
-					
 
 
 
